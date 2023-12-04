0493df53100f42ecb949f39393867401167cfe15
Assanalikhan <210103042@stu.sdu.edu.kz>
2023-12-04 21:32:48 +0600

    Third

--- app/src/main/java/com/example/art_space/MainActivity.kt
+++ app/src/main/java/com/example/art_space/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -32,6 +33,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.art_space.ui.theme.Art_SpaceTheme
 import androidx.compose.material3.Button
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,12 +59,18 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.art1)
-
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.art1
+        2 -> R.drawable.art2
+        3 -> R.drawable.art3
+        else -> R.drawable.art4
+    }
     Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceEvenly ,horizontalAlignment = Alignment.CenterHorizontally) {
         Box(modifier = modifier) {
             Image(
-                painter = image,
-                contentDescription = null,
+                painter = painterResource(id = imageResource),
+                contentDescription = result.toString(),
                 contentScale = ContentScale.Crop,
                 modifier = modifier
                     .size(500.dp, 400.dp)
@@ -70,11 +82,11 @@ fun Greeting(modifier: Modifier = Modifier) {
             modifier = modifier,
         )
         Row(modifier = modifier) {
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { result = (1..6).random() }) {
                 Text(stringResource(R.string.previous))
             }
             Spacer(modifier = Modifier.width(130.dp))
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { result = (1..6).random() }) {
                 Text(stringResource(R.string.next))
             }
         }
